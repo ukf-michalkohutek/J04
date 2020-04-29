54c682d8b3906e87eaadfbfaf80816dd63dc5df6
ukf-RichardVanek <61794975+ukf-RichardVanek@users.noreply.github.com>
2020-04-29 15:29:01 +0200

    J04

---
+++ AddressBook2/src/AddressBook/Main.java
@@ -0,0 +1,53 @@
+package AddressBook;
+
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.UUID;
+
+public class Main  {
+
+    public static void main(String[] args) {
+        UUID uuid;
+        HashMap<UUID, Student> Students = new HashMap<>();
+        Scanner vstup = new Scanner(System.in);
+        String meno;
+        String priezvisko;
+        String email;
+        boolean quit = true;
+        System.out.println("Vitajte:");
+        while (quit) {
+            System.out.println("\na meno priezvisko email -> pridá novú osobu\nd uuid -> vymaže osobu s uuid\ne uuid meno priezvisko email -> upraví osobu s uuid\ns uuid -> vráti údaje o osobe s uuid\nl -> vráti údaje o všetkých osobách\nq -> koniec");
+            switch (vstup.next().charAt(0)){
+                case 'a':
+                    uuid = UUID.randomUUID();
+                    meno = vstup.next();
+                    priezvisko = vstup.next();
+                    email = vstup.next();
+                    Students.put(uuid, new Student(meno,priezvisko,email));
+                    break;
+                case 'd':
+                    Students.remove(UUID.fromString(vstup.next()));
+                    break;
+                case 'e':
+                    uuid = UUID.fromString(vstup.next());
+                    meno = vstup.next();
+                    priezvisko = vstup.next();
+                    email = vstup.next();
+                    Students.put(uuid, new Student(meno,priezvisko,email));
+                    break;
+                case 's':
+                    meno = vstup.next();
+                    System.out.println(Students.get(UUID.fromString(meno)).getFirstName() + " " + Students.get(UUID.fromString(meno)).getLastName() + " " + Students.get(UUID.fromString(meno)).getEmail());
+                    break;
+                case 'l':
+                    for(UUID i: Students.keySet()) System.out.println(i + " " + Students.get(i).getFirstName() + " " + Students.get(i).getLastName() + " " + Students.get(i).getEmail());
+                    break;
+                case 'q':
+                    quit = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
